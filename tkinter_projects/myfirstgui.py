57260d4b3137e73525e889e120078c23250427bd
Joshua Almodovar <joshjalmo@gmail.com>
2023-06-01 06:08:12 -0500

    Adding interactivity to the buttons

--- tkinter_projects/myfirstgui.py
+++ tkinter_projects/myfirstgui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import random
 
 window = Tk()
 window.title("My First GUI Program")
@@ -8,9 +9,24 @@ window.minsize(width=500, height=300)
 my_label = Label(text='What\'s up!', font=("Arial", 24, "bold"))
 my_label.pack()
 
+
 # Button
 
-button = Button(text='Click here')
+
+# def button_got_clicked():
+#     button_options = ['Hello World!', 'You\'re cool!', 'I love sushi!', 'Pikmin are cool!']
+#
+#     my_label['text'] = random.choice(button_options)
+def button_got_clicked():
+    my_label['text'] = input.get()
+
+
+button = Button(text='Click here', command=button_got_clicked)
 button.pack()  # Elements need to have pack to show up on screen
+
+# Entry
+input = Entry(width=20)
+input.pack()
+input.get()
 
 window.mainloop()
