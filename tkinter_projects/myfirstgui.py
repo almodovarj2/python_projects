52dea024795a2545736c60d37e4819958fe9683d
Joshua Almodovar <joshjalmo@gmail.com>
2023-06-01 05:57:57 -0500

    Adding a label and a button to the Gui

--- tkinter_projects/myfirstgui.py
+++ tkinter_projects/myfirstgui.py
@@ -1,6 +1,16 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
+
+# Label
+my_label = Label(text='What\'s up!', font=("Arial", 24, "bold"))
+my_label.pack()
+
+# Button
+
+button = Button(text='Click here')
+button.pack()  # Elements need to have pack to show up on screen
+
 window.mainloop()
