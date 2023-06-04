3541620d84fa98fcbdff5963d9ab104c0d549a12
Joshua Almodovar <joshjalmo@gmail.com>
2023-06-04 08:21:34 -0400

    Adding countdown functionality to the pomodoro timer.

--- tkinter_projects/Pomodoro Timer/main.py
+++ tkinter_projects/Pomodoro Timer/main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import math
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -14,25 +15,30 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 reps = 0
+
+
 def start_timer():
+    global reps
+    reps += 1
     if reps == 8:
         timer_text['text'] = 'Long Break'
         count_down(LONG_BREAK_MIN * 60)
 
     elif reps % 2 == 0:
-        timer_text['text'] = 'Focus'
-        count_down(WORK_MIN * 60)
-    else:
         timer_text['text'] = 'Short Break'
         count_down(SHORT_BREAK_MIN * 60)
+    else:
+        timer_text['text'] = 'Focus'
+        count_down(WORK_MIN * 60)
+
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
     count_min = math.floor(count / 60)
     count_sec = count % 60
     canvas.itemconfig(time_text, text=f"{count_min}:{count_sec:02}")
-    if count >0:
+    if count > 0:
         window.after(1000, count_down, count - 1)
-
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -48,19 +54,18 @@ time_text = canvas.create_text(100, 130, text="00:00", font=(FONT_NAME, "35", 'b
 canvas.grid(column=1, row=1)
 
 # labels
-timer_text = Label(text='Timer',bg=YELLOW, fg='#228B22', font=(FONT_NAME, 40, 'bold'))
+timer_text = Label(text='Timer', bg=YELLOW, fg='#228B22', font=(FONT_NAME, 40, 'bold'))
 timer_text.grid(column=1, row=0)
 
 check_label = Label(text='✓', bg=YELLOW, fg="#228B22", font=('Arial', 20, "bold"))
 check_label.grid(column=1, row=3)
 
 # buttons
-start_button = Button(text='Start', highlightbackground=YELLOW, width=2, font=('Arial', 14, 'bold'), command=start_timer)
+start_button = Button(text='Start', highlightbackground=YELLOW, width=2, font=('Arial', 14, 'bold'),
+                      command=start_timer)
 start_button.grid(column=0, row=2)
-
 
 reset_button = Button(text='Reset', highlightbackground=YELLOW, width=2, font=('Arial', 14, 'bold'))
 reset_button.grid(column=2, row=2)
-
 
 window.mainloop()
