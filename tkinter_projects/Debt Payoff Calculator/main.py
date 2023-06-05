f0b4c82a88807ab5b8ed28f0ec91209e86deec36
Joshua Almodovar <joshjalmo@gmail.com>
2023-06-05 08:37:34 -0400

    Setting up window and continuing skeleton of project

--- tkinter_projects/Debt Payoff Calculator/main.py
+++ tkinter_projects/Debt Payoff Calculator/main.py
@@ -1,10 +1,8 @@
 from tkinter import *
 
 # ---------------------------- CONSTANTS ------------------------------- #
-
-
-
-# ---------------------------- UI SETUP ------------------------------- #
+FONT_NAME = 'Helvetica'
+BG_COLOR = '#E5F9DB'
 
 
 # ---------------------------- CALCULATE DEBT PAYOFF ------------------------------- #
@@ -12,3 +10,25 @@ from tkinter import *
 # ---------------------------- CALCULATE DEBT FREE DATE ------------------------------- #
 
 # ---------------------------- CALCULATE INTEREST PAID ------------------------------- #
+
+# ---------------------------- CALCULATE INTEREST PAID ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Debt Payoff Calculator")
+window.config(padx=100, pady=50, bg=BG_COLOR)
+
+canvas = Canvas(width=300, height=224, bg=BG_COLOR, highlightthickness=0)
+canvas.grid(column=1, row=1)
+
+# LABELS
+# Balance owed
+# Estimated Interest Rate
+# Monthly payment or Desired months to payoff
+
+# BUTTONS
+# Calculate button
+# Start over button that only appears after you click the calculate button the first time
+
+
+window.mainloop()
