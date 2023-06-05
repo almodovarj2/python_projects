aa0af780343ebfa7a79cb83680f534a61492d16d
Joshua Almodovar <joshjalmo@gmail.com>
2023-06-05 14:56:17 -0400

    Building out labels and input boxes.

--- tkinter_projects/Debt Payoff Calculator/main.py
+++ tkinter_projects/Debt Payoff Calculator/main.py
@@ -16,13 +16,17 @@ BG_COLOR = '#E5F9DB'
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Debt Payoff Calculator")
-window.config(padx=100, pady=50, bg=BG_COLOR)
+window.config(padx=100, pady=10, bg=BG_COLOR)
 
-canvas = Canvas(width=300, height=224, bg=BG_COLOR, highlightthickness=0)
+canvas = Canvas(width=300, height=500, bg=BG_COLOR, highlightthickness=0)
 canvas.grid(column=1, row=1)
 
 # LABELS
+title_text = Label(text= 'Debt Repayment Calculator', fg='#83764F', bg= BG_COLOR, font=(FONT_NAME, 30, 'bold'))
+title_text.grid(row=0, column=1)
 # Balance owed
+balance_text = Label(text='Balance Owed',)
+balance_input = Entry(width=10)
 # Estimated Interest Rate
 # Monthly payment or Desired months to payoff
 
