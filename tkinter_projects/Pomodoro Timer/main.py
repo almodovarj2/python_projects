d14ad5b3915189411fb71e45ae5139251e9199c2
Joshua Almodovar <joshjalmo@gmail.com>
2023-06-05 07:39:36 -0400

    Adding Reset button logic and adjusting canvas size.

--- tkinter_projects/Pomodoro Timer/main.py
+++ tkinter_projects/Pomodoro Timer/main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import math
 
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -10,11 +11,22 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+timer = None
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER RESET ------------------------------- #
+def reset():
+    global reps
+    window.after_cancel(timer)
+    check_label.config(text='')
+    timer_text.config(text='Timer', bg=YELLOW, fg='#228B22', font=(FONT_NAME, 40, 'bold'))
+    canvas.itemconfig(time_text, text='00:00')
+    reps = 0
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
-reps = 0
+
 
 def start_timer():
     global reps
@@ -28,7 +40,7 @@ def start_timer():
         count_down(SHORT_BREAK_MIN * 60)
     else:
         timer_text.config(text='Focus', fg='#228B22')
-        count_down(WORK_MIN * 1)
+        count_down(10)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
@@ -37,14 +49,16 @@ def count_down(count):
     count_sec = count % 60
     canvas.itemconfig(time_text, text=f"{count_min}:{count_sec:02}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
     else:
         start_timer()
         marks = ''
-        work_sessions = math.floor(reps/2)
+        work_sessions = math.floor(reps / 2)
         for _ in range(work_sessions):
             marks += '✓'
         check_label.config(text=marks)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -52,10 +66,10 @@ window = Tk()
 window.title("Pomodoro Timer")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+canvas = Canvas(width=300, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file='tomato.png')
-canvas.create_image(100, 112, image=tomato_img)
-time_text = canvas.create_text(100, 130, text="00:00", font=(FONT_NAME, "35", 'bold'))
+canvas.create_image(150, 112, image=tomato_img)
+time_text = canvas.create_text(150, 130, text="00:00", font=(FONT_NAME, "35", 'bold'))
 canvas.grid(column=1, row=1)
 
 # labels
@@ -66,11 +80,11 @@ check_label = Label(text='', bg=YELLOW, fg="#228B22", font=('Arial', 20, "bold")
 check_label.grid(column=1, row=3)
 
 # buttons
-start_button = Button(text='Start', highlightbackground=YELLOW, width=2, font=('Arial', 14, 'bold'),
+start_button = Button(text='Start', highlightbackground=YELLOW, width=4, font=('Arial', 14, 'bold'),
                       command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text='Reset', highlightbackground=YELLOW, width=2, font=('Arial', 14, 'bold'))
+reset_button = Button(text='Reset', highlightbackground=YELLOW, width=4, font=('Arial', 14, 'bold'), command=reset)
 reset_button.grid(column=2, row=2)
 
 window.mainloop()
