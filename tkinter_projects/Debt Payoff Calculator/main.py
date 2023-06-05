8687284a65d6c7ee8a1ff352449b779da08b6dc3
Joshua Almodovar <joshjalmo@gmail.com>
2023-06-05 15:46:03 -0400

    Adding buttons

--- tkinter_projects/Debt Payoff Calculator/main.py
+++ tkinter_projects/Debt Payoff Calculator/main.py
@@ -16,10 +16,8 @@ BG_COLOR = '#E5F9DB'
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Debt Payoff Calculator")
+window.minsize(width=300, height=500)
 window.config(padx=100, pady=10, bg=BG_COLOR)
-
-canvas = Canvas(width=300, height=500, bg=BG_COLOR, highlightthickness=0)
-canvas.grid(column=1, row=1)
 
 # LABELS
 title_text = Label(text= 'Debt Repayment Calculator', fg='#83764F', bg= BG_COLOR, font=(FONT_NAME, 30, 'bold'))
@@ -27,20 +25,21 @@ title_text.grid(row=0, column=1)
 # Balance owed
 balance_text = Label(text='Balance Owed', fg='black', bg=BG_COLOR, font=(FONT_NAME, 20))
 balance_input = Entry(width=10, bg='white', fg='black', highlightthickness=1)
-balance_text.grid(row=1, column=1)
+balance_text.grid(row=1, column=1,pady=(20, 0))
 balance_input.grid(row=2, column=1)
 
 # Estimated Interest Rate
 interest_text = Label(text='Estimated Interest', fg='black', bg=BG_COLOR, font=(FONT_NAME, 20))
 interest_input = Entry(width=10, bg='white', fg='black',highlightthickness=1)
-interest_text.grid(row=3, column=1)
+interest_text.grid(row=3, column=1, pady=(20, 0))
 interest_input.grid(row=4, column=1)
 # Monthly payment or Desired months to payoff
 
 # BUTTONS
 # Calculate button
-calculate_button =
+calculate_button = Button(text='Calculate', bg=BG_COLOR, highlightbackground=BG_COLOR, font=(FONT_NAME, 20, 'bold'))
+calculate_button.grid(row=5, column=1, padx=(0, 150), pady=(20, 0))
 # Start over button that only appears after you click the calculate button the first time
-
-
+start_over = Button(text='Start Over', bg=BG_COLOR, highlightbackground=BG_COLOR, font=(FONT_NAME, 20, 'bold'))
+start_over.grid(row=5, column=1, padx=(150, 0), pady=(20, 0))
 window.mainloop()
