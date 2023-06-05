bdc31c82d9bb17889ab667408bad2823b21aedcb
Joshua Almodovar <joshjalmo@gmail.com>
2023-06-05 15:24:26 -0400

    Updating labels

--- tkinter_projects/Debt Payoff Calculator/main.py
+++ tkinter_projects/Debt Payoff Calculator/main.py
@@ -11,7 +11,7 @@ BG_COLOR = '#E5F9DB'
 
 # ---------------------------- CALCULATE INTEREST PAID ------------------------------- #
 
-# ---------------------------- CALCULATE INTEREST PAID ------------------------------- #
+# ---------------------------- CALCULATE MONTHS ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -26,18 +26,20 @@ title_text = Label(text= 'Debt Repayment Calculator', fg='#83764F', bg= BG_COLOR
 title_text.grid(row=0, column=1)
 # Balance owed
 balance_text = Label(text='Balance Owed', fg='black', bg=BG_COLOR, font=(FONT_NAME, 20))
-balance_input = Entry(width=10)
+balance_input = Entry(width=10, bg='white', fg='black', highlightthickness=1)
 balance_text.grid(row=1, column=1)
 balance_input.grid(row=2, column=1)
+
 # Estimated Interest Rate
 interest_text = Label(text='Estimated Interest', fg='black', bg=BG_COLOR, font=(FONT_NAME, 20))
-interest_input = Entry(width=10)
+interest_input = Entry(width=10, bg='white', fg='black',highlightthickness=1)
 interest_text.grid(row=3, column=1)
 interest_input.grid(row=4, column=1)
 # Monthly payment or Desired months to payoff
 
 # BUTTONS
 # Calculate button
+calculate_button =
 # Start over button that only appears after you click the calculate button the first time
 
 
